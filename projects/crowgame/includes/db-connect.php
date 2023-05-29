d19ce487375eefe2d372b2c9b547b593f363736f
alys <90273749+sernoskie@users.noreply.github.com>
2023-05-28 22:57:31 -0400

    linking projects
    linking project links and adding resume view and download

--- projects/crowgame/includes/db-connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dsn = "mysql:host=sernoski.dev.fast.sheridanc.on.ca;dbname=sernoski_crowgame;charset=utf8mb4";
-
-$dbusername = "sernoski_crowgame";
-$dbpassword = "HANC!IT9vc(=";
-
-$pdo = new PDO($dsn, $dbusername, $dbpassword);
-?>
--- projects/crowgame/includes/session-check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION["userId"])) {
-
-    // makes the code stop right here
-    // exit();
-    header("Location: login.php");
-}
